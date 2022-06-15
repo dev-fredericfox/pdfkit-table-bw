96f31f7909125254465be11d68304f1fb2025d1d
Frederic Fox <dev.frederic.fox@gmail.com>
2022-06-15 09:36:04 +0200

    More test

--- index.js
+++ index.js
@@ -790,10 +790,11 @@ class PDFDocumentWithTables extends PDFDocument {
                   ))
                 }
                 if (text > 0) {
-                  replacedArray.push('text(splitByRegexArray[text])')
+                  replacedArray.push('.text(splitByRegexArray[text])')
                 }
               }
-              splitByRegexArray[0].eval(splitByRegexArray[1])
+              splitByRegexArray[0]
+              eval(splitByRegexArray[1])
               eval(splitByRegexArray[0])
             } else {
               // regex word between two starts.
