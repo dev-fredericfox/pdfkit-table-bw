1d73cd59b2bd5c3d6603942b95c4a4eec1fdb92b
Frederic Fox <dev.frederic.fox@gmail.com>
2022-06-16 11:11:58 +0200

    add new line console log

--- index.js
+++ index.js
@@ -787,20 +787,8 @@ class PDFDocumentWithTables extends PDFDocument {
               const splitByRegexArray = line.split(boldRegexPattern);
               console.log("splitByRegexArray Array:", splitByRegexArray);
               splitByRegexArray.forEach((item, index) => {
-                console.log("For Each Loop Start: ",item)
-                if (index === 0 && splitByRegexArray.length > 1) {
-                  console.log("Index 0:", item);
-                  this.text(
-                    item,
-                    lastPositionX + cellPadding.left,
-                    startY + topTextToAlignVertically,
-                    {
-                      width: width - (cellPadding.left + cellPadding.right),
-                      align: align,
-                      continued: true,
-                    }
-                  );
-                } else if (index % 2 === 1) {
+                console.log("For Each Loop Start: ", item);
+                if (index % 2 === 1) {
                   if (index + 1 === splitByRegexArray.length) {
                     console.log("Bold and last segment:", item);
                     this.font(options.fontBold)
@@ -823,19 +811,7 @@ class PDFDocumentWithTables extends PDFDocument {
                     this.font(options.fontRegular).text(item, { continued: true });
                   }
                 }
-                if (index === 0 && splitByRegexArray.length > 1) {
-                  console.log("Index 0:", item);
-                  this.text(
-                    item,
-                    lastPositionX + cellPadding.left,
-                    startY + topTextToAlignVertically,
-                    {
-                      width: width - (cellPadding.left + cellPadding.right),
-                      align: align,
-                      continued: true,
-                    }
-                  )
-              }
+              });
               if (lineIndex !== splitByNewLines.length - 1) {
                 console.log("moving down", lineIndex);
                 this.moveDown().font(options.fontRegular);
